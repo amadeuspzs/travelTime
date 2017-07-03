fb6a1e6c01e4f4c96b9aa77fbfb88f9c27915bcf
Amadeus Stevenson <amadeus@decoded.com>
2017-07-03 12:39:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# travelTime
+
+**When is the best time to leave by car?**
+
+A collection of scripts to source and analyze realtime traffic information to make descriptive, predictive and prescriptive decisions on when to leave on a trip, and minimize traffic.
+
+We'll plan ahead and capture realtime traffic data between two locations every 10 minutes, then identify when the best time to leave (quickest) is.
+
+## Sourcing data
+
+This project uses the [Google Maps Directions API](https://developers.google.com/maps/documentation/directions/) (you'll need an API key), queried via [cron jobs](https://en.wikipedia.org/wiki/Cron).
+
+* [travelTime.ipynb](travelTime.ipynb) is an interactive jupyter notebook to query traffic data
+* [travelTime.py](travelTime.py) is a standalone CLI version for running as a cron job
+
+Example crontab:
+
+```*/10 * * * * $HOME/bin/travelTime.py -q "Origin" "Destination" >> $HOME/data/origin-destination.csv```
+
+I recommend you run through the interactive notebook to test out your origin/destination combinations before setting up the crontab.
+
+## Analyzing data
+
+TBD
+
+## Contributing
+
+Python is not my go-to language - any refactoring/improvements welcome via Issues or PRs.
---
+++ travelTime.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+import urllib, json, time, argparse
+
+# Create CLI interface
+parser = argparse.ArgumentParser(description='Capture realtime traffic data')
+parser.add_argument('-q', action='store_true', help='Quiet mode, only output successful csv')
+parser.add_argument('origin', help='Where are you starting your journey')
+parser.add_argument('destination', help='Where are you ending your journey')
+
+args = parser.parse_args()
+
+origin = args.origin
+destination = args.destination
+quiet = args.q
+
+# Enter your Google Maps API key below:
+apiKey=""
+if not apiKey:
+    print "Enter your API key for traffic data!"
+    exit(1)
+
+# Try to grab realtime traffic from Google:
+params = urllib.urlencode(
+    {'origin': origin, 
+     'destination': destination, 
+     'mode': 'driving', 
+     'key': apiKey, 
+     'departure_time':'now'})
+
+url = "https://maps.googleapis.com/maps/api/directions/json?%s" % params
+
+timestamp = int(time.time()) # capture when the request is made
+
+try:
+    response = urllib.urlopen(url)
+except IOError, e:
+    if not quiet:
+    	print e
+    exit(1)
+
+if response.getcode() == 200:
+    data = json.loads(response.read())
+else:
+	if not quiet:
+		print "Error %s" % response.getcode()
+	exit(1)
+
+# Process response data
+if 'error_message' in data:
+    if not quiet:
+        print data['error_message']
+    exit(1)
+
+if not 'routes' in data or len(data['routes']) < 1:
+    if not quiet:
+        print "Route data not returned. Check locations?"
+    exit(1)
+elif 'duration_in_traffic' in data['routes'][0]['legs'][0]:
+    print "%s,%s" % (timestamp, data['routes'][0]['legs'][0]['duration_in_traffic']['value'])
+else:
+    if not quiet:
+    	print "Traffic data not returned"
+    exit(1)
